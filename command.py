1978e471dd08be14c25ec6cb75fde5058081dd1d
NewWorld_ <32818386+INewWorldI@users.noreply.github.com>
2021-07-29 07:00:57 +0900

    feat : rpg game add

--- command.py
+++ command.py
@@ -1,44 +1,44 @@
-import random 
-from discord.ext import commands
-
-bot = commands.Bot(command_prefix='$')
+import random
+from bot import bot
 
 @bot.command()
 #가위바위보 게임 구현 함수
-async def 가위바위보(ctx, arg):
+async def 가위바위보(ctx, *arg):
+    const_list = ['가위', '바위', '보']
+
+    arg = arg[0]
     
-    if arg in ['가위', '바위', '보']:
-        select_var = ['가위', '바위', '보']
-        rnd_var = random.choice(select_var)
-        await ctx.send(f'당신은 {arg}를 냈습니다')
+    if arg not in const_list:
+        await ctx.send('가위, 바위, 보 중에 하나를 입력해주세요')
+        return
 
-        if arg in '가위':
-            await ctx.send(f'봇이 {rnd_var}를 냈습니다')
-            if rnd_var in '가위':
-                await ctx.send('가위 = 가위 :: 당신은 비겼습니다. ')
-            elif rnd_var in '바위':
-                await ctx.send('가위 < 바위 :: 당신은 졌습니다.')
-            elif rnd_var in '보':
-                await ctx.send('가위 > 보 :: 당신은 이겼습니다.')
-            
+    rnd_var = random.choice(const_list)
+    await ctx.send(f'당신은 {arg}를 냈습니다')
 
-        elif arg in '바위':
-            await ctx.send(f'봇이 {rnd_var}를 냈습니다')
-            if rnd_var in '가위':
-                await ctx.send('바위 > 가위 :: 당신은 이겼습니다.')
-            elif rnd_var in '바위':
-                await ctx.send('바위 = 바위 :: 당신은 비겼습니다.')
-            elif rnd_var in '보':
-                await ctx.send('바위 < 보 :: 당신은 졌습니다.')
+    if arg in '가위':
+        await ctx.send(f'봇이 {rnd_var}를 냈습니다')
+        if rnd_var in '가위':
+            await ctx.send('가위 = 가위 :: 당신은 비겼습니다. ')
+        elif rnd_var in '바위':
+            await ctx.send('가위 < 바위 :: 당신은 졌습니다.')
+        elif rnd_var in '보':
+            await ctx.send('가위 > 보 :: 당신은 이겼습니다.')
 
-        elif arg in '보':
-            await ctx.send(f'봇이 {rnd_var}를 냈습니다')
-            if rnd_var in '가위':
-                await ctx.send('가위 > 보 :: 당신은 졌습니다.')
-            elif rnd_var in '바위':
-                await ctx.send('보 > 바위 :: 당신은 이겼습니다.')
-            elif rnd_var in '보':
-                await ctx.send('보 = 보 :: 당신은 비겼습니다.')
+    elif arg in '바위':
+        await ctx.send(f'봇이 {rnd_var}를 냈습니다')
+        if rnd_var in '가위':
+            await ctx.send('바위 > 가위 :: 당신은 이겼습니다.')
+        elif rnd_var in '바위':
+            await ctx.send('바위 = 바위 :: 당신은 비겼습니다.')
+        elif rnd_var in '보':
+            await ctx.send('바위 < 보 :: 당신은 졌습니다.')
 
-    else:
-        print('가위, 바위, 보 중에 하나를 입력해주세요')
+    elif arg in '보':
+        await ctx.send(f'봇이 {rnd_var}를 냈습니다')
+        if rnd_var in '가위':
+            await ctx.send('가위 > 보 :: 당신은 졌습니다.')
+        elif rnd_var in '바위':
+            await ctx.send('보 > 바위 :: 당신은 이겼습니다.')
+        elif rnd_var in '보':
+            await ctx.send('보 = 보 :: 당신은 비겼습니다.')
+
---
+++ rpg_game.py
@@ -0,0 +1,46 @@
+import random
+from bot import bot
+
+# 턴제 RPG 게임 구현
+
+class rpg_ustat: # 개체 플레이어 타입 인스턴스 생성
+    def __init__(self, ulevel, uhp, umana, uattack, udefense, uskill_name, uskill_damage):
+        self.ulevel = ulevel
+        self.uhp = uhp
+        self.umana = umana
+        self.uattack = uattack
+        self.udedense = udefense
+        self.uskill_name = uskill_name
+        self.uskill_damage = uskill_damage
+
+
+class rpg_mstat: # 개체 몬스터 타입 인스턴스 생성
+    def __init__(self, level, hp, mana, attack, defense, skill_name, skill_damage):
+        self.level = level
+        self.hp = hp 
+        self.mana = mana
+        self.attack = attack
+        self.defense = defense
+        self.skill_name = skill_name
+        self.skill_damage = skill_damage
+    
+# 몬스터 설정 생성 
+
+slime = rpg_mstat(level=1, hp=20, mana=40, attack=8, defense=0, skill_name='산성액 분출', skill_damage=20)
+bat = rpg_mstat(2, 20, 50, 10, 0.2, '흡혈', 25)
+goblin = rpg_mstat(5, 50, 100, 25, 0.4, '난타', 40)
+gargoyle = rpg_mstat(20, 100, 170, 50, 0.6, '석화', 250)
+eins = rpg_mstat(40, 1500, 0, 0, 0, '섹드립', 0)
+red_dragon = rpg_mstat(100, 1000, 2500, 350, 0.8, '드래곤 브레스', 550)
+
+@bot.command()
+async def rpg(ctx, *args):
+    await ctx.send('간단하게 구현하는 RPG 게임 시스템 입니다.')
+    await ctx.send('도움말은 없습니다 그냥 적당히 설명듣고 쓰세요')
+
+    if len(args) < 2:
+        await ctx.send('잘못된 명령어 사용입니다. rpg <CMD> <ARG>')
+        return
+        
+    await ctx.send(args[0])
+    await ctx.send(args[1])
