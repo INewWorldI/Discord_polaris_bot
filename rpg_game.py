cfce96811a69c3083095605cbcec464f69f6f410
NewWorld_ <32818386+INewWorldI@users.noreply.github.com>
2021-09-03 17:28:15 +0900

    feat: embed form update

--- rpg_game.py
+++ rpg_game.py
@@ -11,15 +11,21 @@ c = conn.cursor()
 # 턴제 RPG 게임 구현
 
 class user_data: # 개체 플레이어 타입 인스턴스 생성
-    def __init__(self, uuid, ulevel, uhp, umana, uattack, udefense, uskill_name, uskill_damage):
+    def __init__(self, uuid, ulevel, uhp, umana, uattack, udefense, uskill_id):
         self.uuid = uuid
         self.ulevel = ulevel
         self.uhp = uhp
         self.umana = umana
         self.uattack = uattack
         self.udedense = udefense
-        self.uskill_name = uskill_name
-        self.uskill_damage = uskill_damage
+        self.uskill_id = uskill_id
+
+
+class skill_data:
+    def __init__(self, skill_id, skill_name, skill_desc, skill_damage, skill_ctime):
+        self.skill_id = skill_id
+        self.skill_name = skill_name
+        
 
 
 class monstaer_data: # 개체 몬스터 타입 인스턴스 생성
@@ -54,27 +60,42 @@ async def 게임(ctx, *args):
     
     elif args == '내정보':
         
-        sql= "SELECT uuid FROM user_data WHERE uuid=?"
-        c.execute(sql, (ctx.message.author.id,))
+        # DB에서 uuid 테이블의 모든 정보를 가져옴
+        uuid_sql= "SELECT uuid FROM user_data WHERE uuid=?"
+        c.execute(uuid_sql, (ctx.message.author.id,))
         uuid_table = c.fetchall()
         uuid_data = tuple(*uuid_table)
-
-        if not uuid_table:
-            await ctx.send('데이터 정보를 불러오지 못했습니다 관리자에게 문의 하세요.')
 
 
         # uuid 테이블에 유저 데이터가 있는지 체크 없으면 생성
 
         if ctx.message.author.id in uuid_data:
 
-            embed=discord.Embed(title="Sample Embed", url="https://realdrewdata.medium.com/", description="This is an embed that will show how to build an embed and the different components", color=0xFF5733)
-            embed.set_author(name=f"{ctx.message.author.name}", url="https://twitter.com/RealDrewData", icon_url="https://pbs.twimg.com/profile_images/1327036716226646017/ZuaMDdtm_400x400.jpg")
+            # 유저의 아바타 이미지 링크를 임베드에 넣기 위해 설정
+            avatar_url = ctx.message.author.avatar_url
+
+            # 임베드에 각종 스테이터스를 담기 위해 DB에서 조회및 데이터 호출
+            stat_sql = "select * from user_data where uuid=?"
+            c.execute(stat_sql, (ctx.message.author.id,))
+            stat_table = c.fetchall()
+            stat_data = tuple(*stat_table) 
+            
+            embed=discord.Embed(title=f"[Lv.{stat_data[1]}] {ctx.message.author.name}의 정보", description="플레이어의 스테이터스 데이터를 표시했습니다.", color=0xFF5733)
+            embed.set_author(name=f"{ctx.message.author.name}", icon_url=f"{avatar_url}")
+            embed.set_thumbnail(url="https://www.mikufan.com/wp-content/uploads/2021/01/cfm_mikuexpo2021online_2_prev.jpg")
+            embed.add_field(name="체력", value=f"[ +{stat_data[2]} ]", inline=True)
+            embed.add_field(name="마나", value=f"[ +{stat_data[3]} ]", inline=True)
+            embed.add_field(name = chr(173), value = chr(173))
+            embed.add_field(name="공격력", value=f"[ +{stat_data[4]} ]", inline=True)
+            embed.add_field(name="방어력", value=f"[ +{stat_data[5]} ]", inline=True)
+            embed.add_field(name = chr(173), value = chr(173))
+            embed.add_field(name="보유스킬", value=f"패치중...", inline=True)
             await ctx.send(embed=embed)
 
         else:
-             await ctx.send('데이터가 존재하지 않습니다 새 데이터를 생성합니다.')
+             await ctx.send('데이터가 존재하지 않습니다 새 데이터를 생성합니다...')
              conn.execute("INSERT INTO user_data VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (ctx.message.author.id, 1, 20, 20, 8, 0, 0, 0))
-             await ctx.send('데이터 생성에 성공했습니다 명령어를 다시 작성하세요')
+             await ctx.send('완료!! 데이터 생성에 성공했습니다 명령어를 다시 입력하세요')
     
 
 # 만약에 에러가 발생된다면 값을 반환
